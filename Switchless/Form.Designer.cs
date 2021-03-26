cd3e803ec1729ea1e60de9bec47b3b0a63201d21
epic_developer <simonglobdob@gmail.com>
2021-03-26 08:45:26 +0100

    Make it automatically connect to ainu, remove the text box and give Yo-ru credits

---
+++ Switchless/Form.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace Switchless
+{
+    partial class Switchless
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Switchless));
+            this.startOsu = new System.Windows.Forms.Button();
+            this.selectOsu = new System.Windows.Forms.Button();
+            this.exitApp = new System.Windows.Forms.Button();
+            this.switchlessLabel = new System.Windows.Forms.Label();
+            this.cuttingEdgeNoteLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startOsu
+            // 
+            this.startOsu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.startOsu.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.startOsu.FlatAppearance.BorderSize = 0;
+            this.startOsu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startOsu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startOsu.ForeColor = System.Drawing.Color.Transparent;
+            this.startOsu.Location = new System.Drawing.Point(305, 148);
+            this.startOsu.Margin = new System.Windows.Forms.Padding(2);
+            this.startOsu.Name = "startOsu";
+            this.startOsu.Size = new System.Drawing.Size(130, 70);
+            this.startOsu.TabIndex = 0;
+            this.startOsu.TabStop = false;
+            this.startOsu.Text = "Start osu!";
+            this.startOsu.UseVisualStyleBackColor = false;
+            this.startOsu.Click += new System.EventHandler(this.startOsuClick);
+            // 
+            // selectOsu
+            // 
+            this.selectOsu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.selectOsu.FlatAppearance.BorderSize = 0;
+            this.selectOsu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.selectOsu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectOsu.ForeColor = System.Drawing.Color.Transparent;
+            this.selectOsu.Location = new System.Drawing.Point(155, 148);
+            this.selectOsu.Margin = new System.Windows.Forms.Padding(2);
+            this.selectOsu.Name = "selectOsu";
+            this.selectOsu.Size = new System.Drawing.Size(130, 70);
+            this.selectOsu.TabIndex = 1;
+            this.selectOsu.Text = "Select osu!.exe";
+            this.selectOsu.UseVisualStyleBackColor = false;
+            this.selectOsu.Click += new System.EventHandler(this.selectOsuClick);
+            // 
+            // exitApp
+            // 
+            this.exitApp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(39)))), ((int)(((byte)(42)))));
+            this.exitApp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exitApp.FlatAppearance.BorderSize = 0;
+            this.exitApp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitApp.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitApp.ForeColor = System.Drawing.Color.White;
+            this.exitApp.Location = new System.Drawing.Point(564, 0);
+            this.exitApp.Margin = new System.Windows.Forms.Padding(2);
+            this.exitApp.Name = "exitApp";
+            this.exitApp.Size = new System.Drawing.Size(37, 32);
+            this.exitApp.TabIndex = 4;
+            this.exitApp.TabStop = false;
+            this.exitApp.Text = "X";
+            this.exitApp.UseVisualStyleBackColor = false;
+            this.exitApp.Click += new System.EventHandler(this.exitAppClick);
+            // 
+            // switchlessLabel
+            // 
+            this.switchlessLabel.AutoSize = true;
+            this.switchlessLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.switchlessLabel.ForeColor = System.Drawing.Color.White;
+            this.switchlessLabel.Location = new System.Drawing.Point(206, 88);
+            this.switchlessLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.switchlessLabel.Name = "switchlessLabel";
+            this.switchlessLabel.Size = new System.Drawing.Size(189, 39);
+            this.switchlessLabel.TabIndex = 5;
+            this.switchlessLabel.Text = "Switchless";
+            this.switchlessLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.switchlessLabel.Click += new System.EventHandler(this.switchlessLabel_Click);
+            // 
+            // cuttingEdgeNoteLabel
+            // 
+            this.cuttingEdgeNoteLabel.AutoSize = true;
+            this.cuttingEdgeNoteLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cuttingEdgeNoteLabel.ForeColor = System.Drawing.Color.White;
+            this.cuttingEdgeNoteLabel.Location = new System.Drawing.Point(10, 345);
+            this.cuttingEdgeNoteLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.cuttingEdgeNoteLabel.Name = "cuttingEdgeNoteLabel";
+            this.cuttingEdgeNoteLabel.Size = new System.Drawing.Size(269, 13);
+            this.cuttingEdgeNoteLabel.TabIndex = 7;
+            this.cuttingEdgeNoteLabel.Text = "CREDIT: We\'re using Switchless v0.3.0 made by Yo-ru.";
+            this.cuttingEdgeNoteLabel.Click += new System.EventHandler(this.creditLabel);
+            // 
+            // Switchless
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(47)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.cuttingEdgeNoteLabel);
+            this.Controls.Add(this.switchlessLabel);
+            this.Controls.Add(this.exitApp);
+            this.Controls.Add(this.selectOsu);
+            this.Controls.Add(this.startOsu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Switchless";
+            this.Text = "Switchless";
+            this.Load += new System.EventHandler(this.Switchless_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.switchlessMouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.switchlessMouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.switchlessMouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startOsu;
+        private System.Windows.Forms.Button selectOsu;
+        private System.Windows.Forms.Button exitApp;
+        private System.Windows.Forms.Label switchlessLabel;
+        private System.Windows.Forms.Label cuttingEdgeNoteLabel;
+    }
+}
+
---
+++ Switchless/Form.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Switchless
+{
+    public partial class Switchless : Form
+    {
+        // Application variables.
+        private bool mouseDown;
+        private Point lastAppLocation;
+
+        // osu! variables.
+        private bool osuSelected = false;
+        private string osuPath = null;
+
+        public Switchless()
+        {
+            InitializeComponent();
+        }
+
+        // Select osu! button.
+        private void selectOsuClick(object sender, EventArgs e)
+        {
+            // Get file
+            OpenFileDialog fdlg = new OpenFileDialog
+            {
+                Title = "Select your osu!.exe file",
+                InitialDirectory = @"%appdata%/../Local/osu!",
+                FileName = "osu!.exe",
+                Filter = "Exe Files (.exe)|*.exe|All Files (*.*)|*.*",
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+
+            // Set path and set as selected.
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                // Check if file is osu!.exe
+                if (fdlg.FileName.EndsWith("osu!.exe"))
+                {
+                    this.osuPath = fdlg.FileName;
+                    this.osuSelected = true;
+                } else
+                {
+                    this.osuPath = null;
+                    this.osuSelected = false;
+                    // TODO: Display message saying invalid osu! EXE.
+                    MessageBox.Show(
+                        "Invalid file selected!\nPlease select your osu!.exe file.",
+                        "Switchless",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Warning
+                        );
+                }
+                Console.WriteLine("DEBUG: Selected osu! path: " + this.osuPath);
+            }
+
+        }
+
+        // Start osu! button.
+        private void startOsuClick(object sender, EventArgs e)
+        {
+            // No osu!.exe file is selected.
+            if (!this.osuSelected)
+            {
+                // Display message if osu!.exe file isn't selected.
+                MessageBox.Show(
+                        "No osu!.exe file was selected.",
+                        "Switchless",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                        );
+            }
+            // If osu! is selected start osu!.
+            else
+            {
+                // Prepare osu!.exe with arguments.
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    CreateNoWindow = false,
+                    UseShellExecute = false,
+                    FileName = this.osuPath,
+                    Arguments = "-devserver ainu.pw"
+                };
+
+                // Try starting osu!.
+                try
+                {
+                    Process.Start(startInfo);
+                }
+                catch
+                {
+                    // Display message if osu! fails to start.
+                    MessageBox.Show(
+                        "Fatal error when starting osu!.\nContact the developer.",
+                        "Switchless",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error
+                        );
+                }
+                finally
+                {
+                    // Exit application.
+                    Console.WriteLine("DEBUG: Started osu! at: " + this.osuPath + " on: ainu.pw.");
+                    Application.Exit();
+                }
+            }
+        }
+
+        // Custom exit button.
+        private void exitAppClick(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // Custom application movement.
+        private void switchlessMouseDown(object sender, MouseEventArgs e)
+        {
+            this.mouseDown = true;
+            this.lastAppLocation = e.Location;
+        }
+
+        private void switchlessMouseUp(object sender, MouseEventArgs e)
+        {
+            this.mouseDown = false;
+        }
+
+        private void switchlessMouseMove(object sender, MouseEventArgs e)
+        {
+            if (this.mouseDown)
+            {
+                this.Location = new Point((this.Location.X - this.lastAppLocation.X) + e.X, (this.Location.Y - this.lastAppLocation.Y) + e.Y);
+                this.Update();
+            }
+        }
+
+        private void switchlessLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void creditLabel(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Yo-ru/Switchless/tree/0.3.0");
+        }
+
+        private void Switchless_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
